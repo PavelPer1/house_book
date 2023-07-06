c1d578374eec6640e26214de7f98646ef564dbe0
PavelPer1 <lolhyh456@gmail.com>
2023-07-06 21:25:32 +0300

    Сделал раздел: мои книги

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
-from .views import login_view, RegisterView
+from .views import login_view, RegisterView, get_user_books
 
 urlpatterns = [
     path('profile', login_view, name='profile'),
-    path('register', RegisterView.as_view(), name='register')
+    path('register', RegisterView.as_view(), name='register'),
+    path('my-books', get_user_books, name='my-books')
 ]
--- Profile/views.py
+++ Profile/views.py
@@ -1,11 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login
-from django.views.decorators.csrf import csrf_protect, csrf_exempt
+from django.views.generic import FormView
+from django.shortcuts import render
+from reportlab.lib.randomtext import objects
 
+from Exchange.models import Books
 from Profile.forms import RegisterForm
 
 
@@ -22,3 +22,13 @@ class RegisterView(FormView):
     def form_valid(self, form):
         form.save()
         return super().form_valid(form)
+
+
+def get_user_books(request):
+    name_books = []
+    for i in Books.objects.all():
+        name_books += [i.name]
+    content = {
+        'name_books': name_books
+    }
+    return render(request, 'user_books.html', content)

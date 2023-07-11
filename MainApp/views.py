057ae4d3865a13f9d39bb18ee6a56565174b80f6
tok1ss <115881295+tok1ss@users.noreply.github.com>
2023-07-11 22:54:27 +0300

    views.py

--- MainApp/views.py
+++ MainApp/views.py
@@ -3,3 +3,12 @@ from django.shortcuts import render
 
 def get_main(request):
     return render(request, 'index.html')
+    
+def go_who(request):
+    return render(request, 'who.html')
+
+def go_help(request):
+    return render(request, 'help.html')
+
+def go_whom(request):
+    return render(request, 'dliakogo.html')

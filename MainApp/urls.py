71dcba16582cde8703c4d4c92be7894346f27b38
tok1ss <115881295+tok1ss@users.noreply.github.com>
2023-07-11 22:54:57 +0300

    urls.py

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -2,5 +2,11 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('', get_main, name='index')
+    path('', get_main, name='index'),
+    
+    path('whome', go_who, name='who'),
+
+    path('help', go_help, name='helpme'),
+
+    path('whom', go_whom, name='whomme'),
 ]

63e7011de3b97e9667c2fe12bb940bdc3b35bfe8
PavelPer1 <lolhyh456@gmail.com>
2023-07-15 16:34:59 +0300

    /

--- Exchange/views.py
+++ Exchange/views.py
@@ -4,6 +4,9 @@ from Exchange.models import Exchange
 
 
 def get_exchange(request):
+    for f in Exchange.objects.all():
+        if f.status != 'В процессе':
+            f.delete()
     exchange = []
     for i in Exchange.objects.all():
         if i.one_book.user_id == request.user.id or i.two_book.user_id == request.user.id:
